9a8b6c2a9d30f8f25ee66ba368211b3aac1ed5c4
Ptitet <ptitet73@gmail.com>
2025-08-05 23:06:44 +0200

    git config is now handled by home manager

--- users/jules/shared.nix
+++ users/jules/shared.nix
@@ -24,5 +24,21 @@
     enableZshIntegration = true;
   };
 
+  programs.git = {
+    enable = true;
+    userName = "Ptitet";
+    userEmail = "ptitet73@gmail.com";
+    aliases = {
+      s = "status";
+      lg = "!git log --decorate --oneline --graph --all";
+      a = "add";
+      c = "commit -m";
+    };
+    extraConfig = {
+      pull.rebase = true;
+      init.defaultBranch = true;
+    };
+  };
+
   programs.home-manager.enable = true; # Let Home Manager install and manage itself
 }

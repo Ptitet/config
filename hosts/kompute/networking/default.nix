86dbf682ca9506ec1f6c87305aba3a1b41b102e9
Ptitet <ptitet73@gmail.com>
2025-08-07 23:53:26 +0200

    iwd sucks for now (or i just didn't read impala's docs)

--- hosts/kompute/networking/default.nix
+++ hosts/kompute/networking/default.nix
@@ -8,16 +8,17 @@
   networking = {
     networkmanager = {
       enable = true;
-      wifi.backend = "iwd";
+      # wifi.backend = "iwd";
       appendNameservers = [
         "1.1.1.1"
         "1.0.0.1"
       ];
     };
-    wireless.iwd.enable = true;
+    wireless.scanOnLowSignal = false;
+    # wireless.iwd.enable = true;
   };
 
-  environment.systemPackages = with pkgs; [ impala ];
+  # environment.systemPackages = with pkgs; [ impala ];
 
   networking.firewall.enable = true;
 }

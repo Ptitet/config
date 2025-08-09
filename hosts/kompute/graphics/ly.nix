37e4686b0fbe4cd40fd90df542a00fd6b4de8d5a
Ptitet <ptitet73@gmail.com>
2025-08-09 19:33:43 +0200

    changed some ly settings

--- hosts/kompute/graphics/ly.nix
+++ hosts/kompute/graphics/ly.nix
@@ -7,7 +7,13 @@
       animation = "colormix";
       asterisk = "0x2022";
       auth_fails = 1;
+      brightness_down_key = "F11";
+      brightness_down_command = "/run/current-system/sw/bin/brightnessctl -e set +10%";
+      brightness_up_key = "F12";
+      brightness_up_command = "/run/current-system/sw/bin/brightnessctl -n -e set 10%-";
       clear_password = true;
+      numlock = true;
+      xinitrc = null;
     };
   };
 }

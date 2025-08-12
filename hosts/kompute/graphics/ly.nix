a14420d948a1a5d77efef7a4cc9cf07db05d59ad
Ptitet <ptitet73@gmail.com>
2025-08-12 22:52:41 +0200

    fixed messed ly config

--- hosts/kompute/graphics/ly.nix
+++ hosts/kompute/graphics/ly.nix
@@ -7,10 +7,10 @@
       animation = "colormix";
       asterisk = "0x2022";
       auth_fails = 1;
+      brightness_down_cmd = "/run/current-system/sw/bin/brightnessctl -n -e set 10%-";
       brightness_down_key = "F11";
-      brightness_down_command = "/run/current-system/sw/bin/brightnessctl -e set +10%";
+      brightness_up_cmd = "/run/current-system/sw/bin/brightnessctl -e set +10%";
       brightness_up_key = "F12";
-      brightness_up_command = "/run/current-system/sw/bin/brightnessctl -n -e set 10%-";
       clear_password = true;
       numlock = true;
       # xinitrc = null; # not supported by the nixpkgs ly package

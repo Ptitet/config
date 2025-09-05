21daa6412cb9cb54e99cfc04126eb67147341fec
Ptitet <ptitet73@gmail.com>
2025-09-05 23:46:48 +0200

    installed zeditor using programs.zed-editor instead of throwing the zed-editor package in home.packages

--- users/jules/apps/zed.nix
+++ users/jules/apps/zed.nix
@@ -11,18 +11,28 @@ in
   options.my.apps.zed.enable = lib.mkEnableOption "Enables zed";
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      zed-editor
-      nixd # Nix lsp
-      nil # Nix lsp
-      hyprls # Hyprlang lsp
-      package-version-server # package.json lsp
-      vscode-json-languageserver # json lsp
-      yaml-language-server # yaml lsp
-      nixfmt-rfc-style
-      vulkan-tools # required for zed
-      vulkan-loader # required for zed
-      vulkan-validation-layers # required for zed
-    ];
+    programs.zed-editor = {
+      enable = true;
+      extensions = [
+        "nix"
+        "kdl"
+        "basher"
+        "toml"
+      ];
+      extraPackages =
+        with pkgs;
+        let
+          lsps = [
+            nixd # Nix lsp
+            nil # Nix lsp
+            hyprls # Hyprlang lsp
+            package-version-server # package.json lsp
+            vscode-json-languageserver # json lsp
+            yaml-language-server # yaml lsp
+          ];
+          formatters = [ nixfmt-rfc-style ];
+        in
+        lsps ++ formatters;
+    };
   };
 }

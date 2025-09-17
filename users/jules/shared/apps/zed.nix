74c64c9af7b1bba5d2c6e98f560c4899b9dbfc22
Ptitet <ptitet73@gmail.com>
2025-09-17 22:38:13 +0200

    removed extensions to auto install from programs.zed-editor.extensions for Zed because we are now using the symlinked config file instead of the generated one

--- users/jules/shared/apps/zed.nix
+++ users/jules/shared/apps/zed.nix
@@ -13,22 +13,16 @@ in
   config = lib.mkIf cfg.enable {
     programs.zed-editor = {
       enable = true;
-      extensions = [
-        "nix"
-        "kdl"
-        "basher"
-        "toml"
-      ];
       extraPackages =
         with pkgs;
         let
           lsps = [
             nixd # Nix lsp
             nil # Nix lsp
-            hyprls # Hyprlang lsp
             package-version-server # package.json lsp
             vscode-json-languageserver # json lsp
             yaml-language-server # yaml lsp
+            tombi
           ];
           formatters = [ nixfmt-rfc-style ];
         in

82af341a6cdaef553dacca3ad529c57344b986a7
Ptitet <ptitet73@gmail.com>
2025-11-11 18:21:34 +0100

    moved wpaperd config out of nix

--- users/jules/shared/services/wpaperd.nix
+++ users/jules/shared/services/wpaperd.nix
@@ -8,19 +8,6 @@ in
   config = lib.mkIf cfg.enable {
     services.wpaperd = {
       enable = true;
-      settings =
-        let
-          path = "/home/jules/wallpapers";
-          random-cfg = group: {
-            inherit path group;
-            duration = "1min";
-            mode = "center";
-          };
-        in
-        {
-          eDP-1 = random-cfg 0;
-          HDMI-A-1 = random-cfg 0;
-        };
     };
   };
 }

df3d8d2b412a346fee2caec4be59aabe0722d4f7
Ptitet <ptitet73@gmail.com>
2025-11-18 22:33:49 +0100

    added ruff in zed lsps

--- users/jules/shared/apps/zed.nix
+++ users/jules/shared/apps/zed.nix
@@ -14,19 +14,24 @@ in
     programs.zed-editor = {
       enable = true;
     };
-    home.packages = with pkgs;
-    let
-      lsps = [
-        nixd # Nix lsp
-        nil # Nix lsp
-        package-version-server # package.json lsp
-        vscode-json-languageserver # json lsp
-        yaml-language-server # yaml lsp
-        tombi
-        tinymist
-      ];
-      formatters = [ nixfmt-rfc-style kdlfmt ];
-    in
-    lsps ++ formatters;
+    home.packages =
+      with pkgs;
+      let
+        lsps = [
+          nixd # Nix lsp
+          nil # Nix lsp
+          package-version-server # package.json lsp
+          vscode-json-languageserver # json lsp
+          yaml-language-server # yaml lsp
+          tombi
+          tinymist
+          ruff
+        ];
+        formatters = [
+          nixfmt-rfc-style
+          kdlfmt
+        ];
+      in
+      lsps ++ formatters;
   };
 }

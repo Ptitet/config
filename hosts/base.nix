402cda6745ddb7f689e42934a0193b3f6dd82fe7
Ptitet <ptitet73@gmail.com>
2025-12-17 23:11:17 +0100

    added keyboard layout switcher

--- hosts/base.nix
+++ hosts/base.nix
@@ -44,7 +44,10 @@
 
   time.timeZone = "Europe/Paris";
 
-  i18n.defaultLocale = "fr_FR.UTF-8";
+  i18n = {
+    defaultLocale = "fr_FR.UTF-8";
+    extraLocales = [ "en_US.UTF-8/UTF-8" ];
+  };
   console = {
     keyMap = "fr-latin1";
   };

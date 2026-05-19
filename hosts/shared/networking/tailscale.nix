a1a11e0e316609cf9ea30a183e38bff1a0719e0b
Ptitet <ptitet73@gmail.com>
2026-05-19 22:14:50 +0200

    formatted tailscale.nix

--- hosts/shared/networking/tailscale.nix
+++ hosts/shared/networking/tailscale.nix
@@ -1,15 +1,16 @@
 { config, ... }:
 {
-  services.tailscale = {
-    enable = true;
+  services.tailscale.enable = true;
+  networking = {
+    nftables.enable = true;
+    firewall = {
+      trustedInterfaces = [ "tailscale0" ];
+      allowedUDPPorts = [ config.services.tailscale.port ];
+    };
   };
-
-  networking.nftables.enable = true;
-  networking.firewall = {
-    trustedInterfaces = [ "tailscale0" ];
-    allowedUDPPorts = [ config.services.tailscale.port ];
+  systemd = {
+    services.tailscaled.serviceConfig.Environment = [ "TS_DEBUG_FIREWALL_MODE=nftables" ];
+    network.wait-online.enable = false;
   };
-  systemd.services.tailscaled.serviceConfig.Environment = [ "TS_DEBUG_FIREWALL_MODE=nftables" ];
-  systemd.network.wait-online.enable = false;
   boot.initrd.systemd.network.wait-online.enable = false;
 }
